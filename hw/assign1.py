dd550504c54c2df833c37f3f6b709ef9eee45a4e
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-05-06 14:34:31 +0900

    finalizing plots

--- hw/assign1.py
+++ hw/assign1.py
@@ -9,7 +9,14 @@ import math
 from matplotlib import pyplot, cm
 from mpl_toolkits.mplot3d import Axes3D
 
-def fem(nx=11, ny=11, nit=100, draw=False) :
+# Discrete solution for 2D Laplace's solution
+def fdm(nx=11, ny=11, nit=100, draw=False) :
+  '''
+  nx : x-axis spartial resolution
+  ny : y-axis spartial resolution
+  nit : number of time step
+  draw : plot the result
+  '''
   dx = 2./(nx-1)
   dy = 1./(ny-1)
   x = np.linspace(0,2,nx)
@@ -40,74 +47,29 @@ def fem(nx=11, ny=11, nit=100, draw=False) :
     pyplot.show()
   return p
 
+# Exact boundary solution for 2D Laplace's equation
 def exact(n=64, inf=101, draw=False) :
+  '''
+  n : number of boundary points
+  int : pseudo infinity (101 is maximum for sinh func)
+  draw : plot the final result
+  '''
   pi = math.pi
   x = np.zeros(n)
   y = np.zeros(n)
-  u = np.zeros(n)
-  bc = np.zeros(n)
   for i in range(0,n):
     if i < (n/4) :
       x[i] = i*8./n
       y[i] = 0
-      u[i] = 0
-      bc[i] = 1
     elif i < n/2 + 1 :
       x[i] = 2
       y[i] = (i-n/4)*4./n
-      u[i] = (i-n/4)*4./n
-      bc[i] = 0
     elif i < 3*n/4:
       x[i] = (3*n/4-i)*8./n
       y[i] = 1
-      u[i] = 0
-      bc[i] = 1
     else :
       x[i] = 0
       y[i] = (n-i)*4./n
-      u[i] = 0
-      bc[i] = 0
-  ip1 = np.arange(n)
-  ip1 += 1
-  ip1[n-1] = 0
-
-  xm = 0.5 * (x+x[ip1])
-  ym = 0.5 * (y+y[ip1])
-  dx = x[ip1] - x
-  dy = y[ip1] - y
-  d = np.zeros(n)
-  
-  for i in range(0,n):
-    d[i] = math.sqrt(dx[i]*dx[i] + dy[i]*dy[i])
-
-  G = np.zeros((n,n))
-  H = np.zeros((n,n))
-  for i in range(0,n):
-    for j in range(0,n):
-      if i != j:
-        rx = xm[i] - xm[j]
-        ry = ym[i] - ym[j]
-        r = math.sqrt(rx*rx + ry*ry)
-        G[i,j] = -math.log(r) * d[j]/2/pi
-        H[i,j] = (rx*dy[j] - ry*dx[j])/r/r/2/pi
-    G[i,i] = d[i] * (1-math.log(d[i]/2))/2/pi
-    H[i,i] = 0.5
-
-  for i in range(0,n):
-    if bc[i] == 1:
-      tmp = G[:,i]
-      G[:,i] = -H[:,i]
-      H[:,i] = -tmp
-
-  b = H.dot(u)
-
-  un = np.linalg.solve(G,b)
-
-  for i in range(0,n):
-    if bc[i] == 1:
-      tmp = u[i]
-      u[i] = un[i]
-      un[i] = tmp
 
   ux = np.zeros(n)
   for i in range(0,n):
@@ -126,8 +88,14 @@ def exact(n=64, inf=101, draw=False) :
     pyplot.show()
   return x,y,ux
 
-
+# Plot 2 result p, q in the same figure
 def scatter_plot(x,y,p,q):
+  '''
+  x : x-axis
+  y : y-axis
+  p : 1D boundary (exact) result
+  q : 1D boundary (approximate) result
+  '''
   fig = pyplot.figure(figsize=(11,7), dpi=100)
   ax = fig.gca(projection='3d')
   ax.scatter(x,y,p,c='b')
@@ -136,7 +104,11 @@ def scatter_plot(x,y,p,q):
   ax.view_init(elev=40., azim=-130.)
   pyplot.show()
 
+# Get border result from 2D fdm method
 def get_border(a):
+  '''
+  a : 2D result from fdm method
+  '''
   size = a.shape
   length = size[0]
   size = 2*(size[0]+size[1])
@@ -149,8 +121,15 @@ def get_border(a):
   ret[3*length:] = temp[::-1]
   return ret[::-1]
 
+# Compute error between p and q
 def error(p,q,tor=1e-6):
+  '''
+  p : 1D boudnary exact result
+  q : 1D boundary approximate result
+  tor : pseudo zero
+  '''
   assert len(p) == len(q)
+  length = len(p)
   error = 0.0
   for i in range(len(p)):
     d = p[i] - q[i] 
@@ -160,10 +139,16 @@ def error(p,q,tor=1e-6):
     else:
       r = (d**2) / (p[i]**2)
     error += r
-  return math.sqrt(error)
+  return math.sqrt(error) / math.sqrt(length)
 
+# Compute alternative relative square error
 def error_rel(p,q):
+  '''
+  p : 1D boundary exact result
+  q : 1D boundary approximate result
+  '''
   assert len(p) == len(q)
+  length = len(p)
   error = 0.0
   for i in range(len(p)):
     d = p[i] - q[i]
@@ -171,8 +156,9 @@ def error_rel(p,q):
     if d != 0:
       r = (d**2) / ((p[i] + q[i])**2)
     error += r
-  return math.sqrt(error)
+  return math.sqrt(error) / math.sqrt(length)
 
+# Experimental: Compute sum error
 def error_rat(p,q,tor=1e-8):
   assert len(p) == len(q)
   sum1 = 0.0
